98442b8651708bdcceb014a614bee9f5876b3c27
Altynbek Quat <65371350+altynbeq@users.noreply.github.com>
2023-06-08 20:06:20 +0200

    challenge 3 of 3: conditional rendering
    Refactor a series of ? : to if and variables

---
+++ conditionChallenges3.jsx
@@ -0,0 +1,33 @@
+function Drink({ name }) {
+  if( name == 'tea'){
+    part = 'leaf'
+    content = '15–70 mg/cup'
+    age = '4,000+ years'
+  } else {
+    part = 'bean'
+    content = '80–185 mg/cup'
+    age = '1,000+ years'
+  }
+  return (
+    <section>
+      <h1>{name}</h1>
+      <dl>
+        <dt>Part of plant</dt>
+        <dd>{part}</dd>
+        <dt>Caffeine content</dt>
+        <dd>{content}</dd>
+        <dt>Age</dt>
+        <dd>{age}</dd>
+      </dl>
+    </section>
+  );
+}
+
+export default function DrinkList() {
+  return (
+    <div>
+      <Drink name="tea" />
+      <Drink name="coffee" />
+    </div>
+  );
+}
